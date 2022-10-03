063c448fb9a79404c48db7bf3735ddad65f3fda2
unknown <jerald commit>
2022-10-03 20:46:24 +0800

    commit

--- learning_django/settings.py
+++ learning_django/settings.py
@@ -142,6 +142,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
